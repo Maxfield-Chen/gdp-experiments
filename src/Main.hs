9773059bc3e5f686146e6d3b828a7a93ca0fc0ed
maxfield-chen <maxfieldchen@gmail.com>
2020-02-17 23:51:20 -0800

    Fix resolver version / hie issues. Reformat code. Play with implicit proof passing.

--- src/Main.hs
+++ src/Main.hs
@@ -1,40 +1,52 @@
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 
 
 module Main where
 
-import Prelude hiding (head)
-import Theory.Lists
-import Theory.Named
-import Logic.Proof
-import Data.The
-import Data.Coerce
-import qualified Data.List as L
-import Data.Ord
+import           Prelude
+import           Theory.Lists
+import           Theory.Named
+import           Logic.Proof
+import           Data.The
+import           Data.Refined
+import           Data.Coerce
+import qualified Data.List                     as L
+import           Data.Ord
+import           Logic.Implicit
 
 newtype SortedBy comp a = SortedBy a
 instance The (SortedBy comp a) a
 
-sortBy :: ((a -> a -> Ordering) ~~ comp)
-       -> [a]
-       -> SortedBy comp [a]
+sortBy :: ((a -> a -> Ordering) ~~ comp) -> [a] -> SortedBy comp [a]
 sortBy comp xs = coerce (L.sortBy (the comp) xs)
 
-mergeBy :: Ord a => ((a -> a -> Ordering) ~~ comp) 
-         -> SortedBy comp [a]
-         -> SortedBy comp [a]
-         -> SortedBy comp [a]
-mergeBy comp xs ys = coerce $ foldr (\(x,y) ret ->
-    if the comp x y == LT then x:y:ret else y:x:ret) [] (zip (the xs) (the ys))
+mergeBy
+  :: Ord a
+  => ((a -> a -> Ordering) ~~ comp)
+  -> SortedBy comp [a]
+  -> SortedBy comp [a]
+  -> SortedBy comp [a]
+mergeBy comp xs ys = coerce $ foldr
+  (\(x, y) ret -> if the comp x y == LT then x : y : ret else y : x : ret)
+  []
+  (zip (the xs) (the ys))
+
+gdpHead :: Fact (IsCons xs) => ([a] ~~ xs) -> a
+gdpHead xs = L.head (the xs)
 
 main :: IO ()
 main = do
   xs <- readLn :: IO [Int]
   ys <- readLn
-  name (comparing Down) $ \gt -> do 
-    let xs' = sortBy gt xs
-        ys' = sortBy gt ys
-    print (the (mergeBy gt xs' ys'))
+  name (comparing Down) $ \gt -> do
+    let xs'    = sortBy gt xs
+        ys'    = sortBy gt ys
+        sorted = the (mergeBy gt xs' ys')
+    name sorted $ \sorted -> case classify' sorted of
+      Cons_ h t -> print (the h)
+      Nil_      -> main
+
 
